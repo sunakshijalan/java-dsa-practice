04c1861fd83ac8a639f48b772b890b0ddd07b953
Sunakshi Jalan <sunakshijalan1@gmail.com>
2026-06-09 12:25:09 +0530

    Add FirstLastOccurrence

---
+++ Recursion/FirstLastOccurrence.java
@@ -0,0 +1,26 @@
+public class FirstLastOccurrence 
+{
+    void occurence(String s,char a,int i,int f,int l)
+    {
+        if(i==s.length()-1)
+            System.out.println("last occurence of character is "+l);
+        else
+        {
+            if(s.charAt(i)==a&&f==-1)
+            {
+                System.out.println("The first occurence of character is "+i);
+                occurence(s,a,i+1,i,l);
+            }
+            else if(s.charAt(i)==a&&f!=-1)
+                occurence(s,a,i+1,f,i);
+            else
+                occurence(s,a,i+1,f,l);
+        }
+        
+    }
+    public static void main()
+    {
+        FirstLastOccurrence ob=new FirstLastOccurrence();
+        ob.occurence("abaacdaefaah", 'a', 0, -1, -1);
+    }
+}
